556e972b996b14648f1d9802af839ff28f80944f
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2023-07-24 17:28:47 +0530

    Changing direction

--- core/urls.py
+++ core/urls.py
@@ -19,4 +19,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:page>/", views.index, name="index"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django_htmx.http import retarget
 
 # Create your views here.
-def index(request):
-    return render(request, "index.html")
+def index(request, page=''):
+    if not page:
+        return HttpResponseRedirect('home')
+    if request.htmx:
+        response = render(request, f"{page}.html")
+        return retarget(response, "#page-content")
+    return render(request, "index.html", {"content": f"{page}.html"})
+
